4830da7c04ac13f1c3ade005a734fec78630a6e5
GGRei <GGRei@users.noreply.github.com>
2026-06-09 10:21:31 +0200

    v2: cover x64 bst on macos windows

--- vlib/v2/gen/x64/x64_runtime_smoke_test.v
+++ vlib/v2/gen/x64/x64_runtime_smoke_test.v
@@ -5854,6 +5854,11 @@ fn test_x64_macos_windows_tree_of_nodes_example_top_level_stdout_exact_bytes() {
 		x64_examples_dir(), 'tree_of_nodes.v', x64_tree_of_nodes_example_stdout())
 }
 
+fn test_x64_macos_windows_binary_search_tree_example_stdout_matches_v_run() {
+	assert_x64_macos_windows_file_stdout_matches_v_run('binary_search_tree_example_top_level_v_run',
+		x64_examples_dir(), 'binary_search_tree.v')
+}
+
 fn test_x64_macos_windows_bfs_example_top_level_stdout_exact_bytes() {
 	assert_x64_macos_windows_file_stdout_bytes('bfs_example_top_level_exact', os.join_path(x64_examples_dir(),
 		'graphs'), 'bfs.v', x64_bfs_example_stdout())
