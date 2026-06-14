33e878547f5e01562a17c4e012397cd42ade3d16
GGRei <tontonalex_887@hotmail.com>
2026-06-14 23:14:59 +0200

    v2: refactor autofree cleanup preview tests

--- vlib/v2/gen/cleanc/autofree_statement_cleanup_preview_test.v
+++ vlib/v2/gen/cleanc/autofree_statement_cleanup_preview_test.v
@@ -28,6 +28,11 @@ fn autofree_statement_cleanup_preview_test_assert_no_preview(slots []AutofreeCle
 	assert previews.len == 0
 }
 
+fn autofree_statement_cleanup_preview_test_assert_no_named_preview(case_name string, slots []AutofreeCleanCStatementEmissionSlotFact) {
+	assert case_name.len > 0
+	autofree_statement_cleanup_preview_test_assert_no_preview(slots)
+}
+
 fn test_autofree_statement_cleanup_preview_accepts_normal_name() {
 	slot := autofree_statement_cleanup_preview_test_slot()
 	previews := autofree_statement_cleanup_preview_facts_from_slots([slot])
@@ -85,118 +90,91 @@ fn test_autofree_statement_cleanup_preview_rejects_two_slots() {
 	autofree_statement_cleanup_preview_test_assert_no_preview([slot, slot])
 }
 
-fn test_autofree_statement_cleanup_preview_rejects_unknown_status() {
-	slot := AutofreeCleanCStatementEmissionSlotFact{
-		...autofree_statement_cleanup_preview_test_slot()
-		slot_status: .unknown
+fn test_autofree_statement_cleanup_preview_rejects_invalid_slot_fields() {
+	base := autofree_statement_cleanup_preview_test_slot()
+	cases := [
+		'unknown_status',
+		'unknown_kind',
+		'empty_fn_key',
+		'empty_fn_name',
+		'empty_name',
+		'invalid_fn_ids',
+		'invalid_target_ids',
+		'invalid_stmt_ids',
+		'invalid_insert_after_ids',
+		'stmt_node_mismatch',
+		'stmt_pos_mismatch',
+		'target_equal_insert_after',
+		'negative_stmt_index',
+		'nonzero_lhs_index',
+	]
+	slots := [
+		AutofreeCleanCStatementEmissionSlotFact{
+			...base
+			slot_status: .unknown
+		},
+		AutofreeCleanCStatementEmissionSlotFact{
+			...base
+			slot_kind: .unknown
+		},
+		AutofreeCleanCStatementEmissionSlotFact{
+			...base
+			fn_key: ''
+		},
+		AutofreeCleanCStatementEmissionSlotFact{
+			...base
+			fn_name: ''
+		},
+		AutofreeCleanCStatementEmissionSlotFact{
+			...base
+			name: ''
+		},
+		AutofreeCleanCStatementEmissionSlotFact{
+			...base
+			fn_node_id: ast.FlatNodeId(-1)
+			fn_pos_id:  0
+		},
+		AutofreeCleanCStatementEmissionSlotFact{
+			...base
+			target_node_id: ast.FlatNodeId(-1)
+			target_pos_id:  0
+		},
+		AutofreeCleanCStatementEmissionSlotFact{
+			...base
+			stmt_node_id: ast.FlatNodeId(-1)
+			stmt_pos_id:  0
+		},
+		AutofreeCleanCStatementEmissionSlotFact{
+			...base
+			insert_after_node_id: ast.FlatNodeId(-1)
+			insert_after_pos_id:  0
+		},
+		AutofreeCleanCStatementEmissionSlotFact{
+			...base
+			insert_after_node_id: ast.FlatNodeId(31)
+		},
+		AutofreeCleanCStatementEmissionSlotFact{
+			...base
+			insert_after_pos_id: 211
+		},
+		AutofreeCleanCStatementEmissionSlotFact{
+			...base
+			target_node_id: ast.FlatNodeId(30)
+		},
+		AutofreeCleanCStatementEmissionSlotFact{
+			...base
+			stmt_index: -1
+		},
+		AutofreeCleanCStatementEmissionSlotFact{
+			...base
+			lhs_index: 1
+		},
+	]
+	assert cases.len == slots.len
+	for index, invalid_case in cases {
+		slot := slots[index]
+		autofree_statement_cleanup_preview_test_assert_no_named_preview(invalid_case, [
+			slot,
+		])
 	}
-	autofree_statement_cleanup_preview_test_assert_no_preview([slot])
-}
-
-fn test_autofree_statement_cleanup_preview_rejects_unknown_kind() {
-	slot := AutofreeCleanCStatementEmissionSlotFact{
-		...autofree_statement_cleanup_preview_test_slot()
-		slot_kind: .unknown
-	}
-	autofree_statement_cleanup_preview_test_assert_no_preview([slot])
-}
-
-fn test_autofree_statement_cleanup_preview_rejects_empty_fn_key() {
-	slot := AutofreeCleanCStatementEmissionSlotFact{
-		...autofree_statement_cleanup_preview_test_slot()
-		fn_key: ''
-	}
-	autofree_statement_cleanup_preview_test_assert_no_preview([slot])
-}
-
-fn test_autofree_statement_cleanup_preview_rejects_empty_fn_name() {
-	slot := AutofreeCleanCStatementEmissionSlotFact{
-		...autofree_statement_cleanup_preview_test_slot()
-		fn_name: ''
-	}
-	autofree_statement_cleanup_preview_test_assert_no_preview([slot])
-}
-
-fn test_autofree_statement_cleanup_preview_rejects_empty_name() {
-	slot := AutofreeCleanCStatementEmissionSlotFact{
-		...autofree_statement_cleanup_preview_test_slot()
-		name: ''
-	}
-	autofree_statement_cleanup_preview_test_assert_no_preview([slot])
-}
-
-fn test_autofree_statement_cleanup_preview_rejects_invalid_fn_ids() {
-	slot := AutofreeCleanCStatementEmissionSlotFact{
-		...autofree_statement_cleanup_preview_test_slot()
-		fn_node_id: ast.FlatNodeId(-1)
-		fn_pos_id:  0
-	}
-	autofree_statement_cleanup_preview_test_assert_no_preview([slot])
-}
-
-fn test_autofree_statement_cleanup_preview_rejects_invalid_target_ids() {
-	slot := AutofreeCleanCStatementEmissionSlotFact{
-		...autofree_statement_cleanup_preview_test_slot()
-		target_node_id: ast.FlatNodeId(-1)
-		target_pos_id:  0
-	}
-	autofree_statement_cleanup_preview_test_assert_no_preview([slot])
-}
-
-fn test_autofree_statement_cleanup_preview_rejects_invalid_stmt_ids() {
-	slot := AutofreeCleanCStatementEmissionSlotFact{
-		...autofree_statement_cleanup_preview_test_slot()
-		stmt_node_id: ast.FlatNodeId(-1)
-		stmt_pos_id:  0
-	}
-	autofree_statement_cleanup_preview_test_assert_no_preview([slot])
-}
-
-fn test_autofree_statement_cleanup_preview_rejects_invalid_insert_after_ids() {
-	slot := AutofreeCleanCStatementEmissionSlotFact{
-		...autofree_statement_cleanup_preview_test_slot()
-		insert_after_node_id: ast.FlatNodeId(-1)
-		insert_after_pos_id:  0
-	}
-	autofree_statement_cleanup_preview_test_assert_no_preview([slot])
-}
-
-fn test_autofree_statement_cleanup_preview_rejects_stmt_node_mismatch() {
-	slot := AutofreeCleanCStatementEmissionSlotFact{
-		...autofree_statement_cleanup_preview_test_slot()
-		insert_after_node_id: ast.FlatNodeId(31)
-	}
-	autofree_statement_cleanup_preview_test_assert_no_preview([slot])
-}
-
-fn test_autofree_statement_cleanup_preview_rejects_stmt_pos_mismatch() {
-	slot := AutofreeCleanCStatementEmissionSlotFact{
-		...autofree_statement_cleanup_preview_test_slot()
-		insert_after_pos_id: 211
-	}
-	autofree_statement_cleanup_preview_test_assert_no_preview([slot])
-}
-
-fn test_autofree_statement_cleanup_preview_rejects_target_equal_insert_after() {
-	slot := AutofreeCleanCStatementEmissionSlotFact{
-		...autofree_statement_cleanup_preview_test_slot()
-		target_node_id: ast.FlatNodeId(30)
-	}
-	autofree_statement_cleanup_preview_test_assert_no_preview([slot])
-}
-
-fn test_autofree_statement_cleanup_preview_rejects_negative_stmt_index() {
-	slot := AutofreeCleanCStatementEmissionSlotFact{
-		...autofree_statement_cleanup_preview_test_slot()
-		stmt_index: -1
-	}
-	autofree_statement_cleanup_preview_test_assert_no_preview([slot])
-}
-
-fn test_autofree_statement_cleanup_preview_rejects_nonzero_lhs_index() {
-	slot := AutofreeCleanCStatementEmissionSlotFact{
-		...autofree_statement_cleanup_preview_test_slot()
-		lhs_index: 1
-	}
-	autofree_statement_cleanup_preview_test_assert_no_preview([slot])
 }
