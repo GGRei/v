7d4bde59bc9de7ecd70b51abd0028bce66d55b7e
GGRei <tontonalex_887@hotmail.com>
2026-07-28 20:53:01 +0200

    tools: use portable script flags on OpenBSD

--- cmd/tools/vwatch_stdin_test.v
+++ cmd/tools/vwatch_stdin_test.v
@@ -3,6 +3,15 @@ import time
 
 const vwatch_stdin_vexe = @VEXE
 
+fn openbsd_script_args(command string) []string {
+	return ['-c', command, '/dev/null']
+}
+
+fn test_openbsd_script_args() {
+	command := "'/bin/sh' -m '/tmp/run.sh' '/tmp/helper'"
+	assert openbsd_script_args(command) == ['-c', command, '/dev/null']
+}
+
 fn test_background_watch_does_not_take_terminal() {
 	$if windows {
 		return
@@ -29,11 +38,13 @@ fn test_background_watch_does_not_take_terminal() {
 		mut process := os.new_process(script_exe)
 		process.set_redirect_stdio()
 		process.use_pgroup = true
-		$if macos || freebsd || openbsd {
+		command := '${os.quoted_path(shell_exe)} -m ${os.quoted_path(shell_path)} ${os.quoted_path(helper_path)} ${os.quoted_path(sleep_exe)} ${os.quoted_path(marker_path)}'
+		$if openbsd {
+			process.set_args(openbsd_script_args(command))
+		} $else $if macos || freebsd {
 			process.set_args(['-q', '/dev/null', shell_exe, '-m', shell_path, helper_path, sleep_exe,
 				marker_path])
 		} $else {
-			command := '${os.quoted_path(shell_exe)} -m ${os.quoted_path(shell_path)} ${os.quoted_path(helper_path)} ${os.quoted_path(sleep_exe)} ${os.quoted_path(marker_path)}'
 			process.set_args(['-q', '-c', command, '/dev/null'])
 		}
 		process.run()
@@ -83,11 +94,13 @@ fn test_background_restore_does_not_reclaim_terminal() {
 		mut process := os.new_process(script_exe)
 		process.set_redirect_stdio()
 		process.use_pgroup = true
-		$if macos || freebsd || openbsd {
+		command := '${os.quoted_path(shell_exe)} -m ${os.quoted_path(shell_path)} ${os.quoted_path(helper_path)} ${os.quoted_path(marker_path)}'
+		$if openbsd {
+			process.set_args(openbsd_script_args(command))
+		} $else $if macos || freebsd {
 			process.set_args(['-q', '/dev/null', shell_exe, '-m', shell_path, helper_path,
 				marker_path])
 		} $else {
-			command := '${os.quoted_path(shell_exe)} -m ${os.quoted_path(shell_path)} ${os.quoted_path(helper_path)} ${os.quoted_path(marker_path)}'
 			process.set_args(['-q', '-c', command, '/dev/null'])
 		}
 		process.run()
@@ -127,11 +140,13 @@ fn test_watch_run_forwards_terminal_input() {
 		mut process := os.new_process(script_exe)
 		process.set_redirect_stdio()
 		process.use_pgroup = true
-		$if macos || freebsd || openbsd {
+		command := '${os.quoted_path(vwatch_stdin_vexe)} watch run ${os.quoted_path(source_path)} ${os.quoted_path(marker_path)}'
+		$if openbsd {
+			process.set_args(openbsd_script_args(command))
+		} $else $if macos || freebsd {
 			process.set_args(['-q', '/dev/null', vwatch_stdin_vexe, 'watch', 'run', source_path,
 				marker_path])
 		} $else {
-			command := '${os.quoted_path(vwatch_stdin_vexe)} watch run ${os.quoted_path(source_path)} ${os.quoted_path(marker_path)}'
 			process.set_args(['-q', '-c', command, '/dev/null'])
 		}
 		process.run()
