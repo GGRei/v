6ca7c410de722d379d54c526af27732bdc5ce4da
GGRei <tontonalex_887@hotmail.com>
2026-08-29 15:10:48 +0200

    compiler: expose static pkgconfig mode to V1 and V3

--- vlib/v3/pref/pref_test.v
+++ vlib/v3/pref/pref_test.v
@@ -42,3 +42,176 @@ fn test_get_module_path_resolves_alias_and_submodule() {
 	assert prefs.get_module_path('modules.legacy.sub', main_file) == os.real_path(os.join_path_single(canonical_dir,
 		'sub'))
 }
+
+struct StaticPkgConfigPrefCase {
+	raw_ccompiler string
+	ccompiler     string
+	cflags        []string
+	ldflags       []string
+	env_cflags    []string
+	env_ldflags   []string
+}
+
+fn static_pkgconfig_preferences(tc StaticPkgConfigPrefCase) &Preferences {
+	mut prefs := new_preferences()
+	prefs.ccompiler = tc.ccompiler
+	prefs.resolve_pkgconfig_mode(tc.raw_ccompiler, tc.cflags, tc.ldflags, tc.env_cflags,
+		tc.env_ldflags)
+	return prefs
+}
+
+fn test_static_pkgconfig_compiler_owned_names_are_exactly_reserved() {
+	assert is_compiler_owned_define('v:static_pkgconfig')
+	assert is_compiler_owned_define('v_static_pkgconfig')
+	assert !is_compiler_owned_define('static_pkgconfig')
+	assert !is_compiler_owned_define('v:user_value')
+	assert !is_compiler_owned_define('v:flag:doc_attr')
+}
+
+fn test_static_pkgconfig_mode_and_canonical_value_matrix() {
+	for tc in [
+		StaticPkgConfigPrefCase{
+			raw_ccompiler: 'gcc'
+			ccompiler:     'gcc'
+			cflags:        ['-static']
+		},
+		StaticPkgConfigPrefCase{
+			raw_ccompiler: 'clang.exe'
+			ccompiler:     'clang'
+			ldflags:       ['-static']
+		},
+		StaticPkgConfigPrefCase{
+			raw_ccompiler: 'x86_64-w64-mingw32-gcc'
+			ccompiler:     'mingw'
+			cflags:        ['-Wno-error', '-static']
+		},
+		StaticPkgConfigPrefCase{
+			raw_ccompiler: 'c++'
+			ccompiler:     'cplusplus'
+			ldflags:       ['-static']
+		},
+	] {
+		prefs := static_pkgconfig_preferences(tc)
+		assert prefs.pkgconfig_mode == .static_, tc.raw_ccompiler
+		assert prefs.compile_values['v:static_pkgconfig'] == 'true', tc.raw_ccompiler
+		assert prefs.file_defines == ['v_static_pkgconfig'], tc.raw_ccompiler
+		assert 'v_static_pkgconfig' in prefs.source_file_defines(), tc.raw_ccompiler
+		assert 'v_static_pkgconfig' !in prefs.user_defines, tc.raw_ccompiler
+	}
+}
+
+fn test_static_pkgconfig_mode_rejects_unsupported_drivers_and_lookalikes() {
+	for tc in [
+		StaticPkgConfigPrefCase{
+			raw_ccompiler: 'tcc'
+			ccompiler:     'tinyc'
+			cflags:        ['-static']
+		},
+		StaticPkgConfigPrefCase{
+			raw_ccompiler: 'cl.exe'
+			ccompiler:     'msvc'
+			ldflags:       ['-static']
+		},
+		StaticPkgConfigPrefCase{
+			raw_ccompiler: 'gcc'
+			ccompiler:     'gcc'
+			cflags:        ['-static-libgcc']
+		},
+		StaticPkgConfigPrefCase{
+			raw_ccompiler: 'gcc'
+			ccompiler:     'gcc'
+			ldflags:       ['-Wl,-Bstatic']
+		},
+		StaticPkgConfigPrefCase{
+			raw_ccompiler: 'gcc'
+			ccompiler:     'gcc'
+			env_cflags:    ['-static']
+		},
+	] {
+		prefs := static_pkgconfig_preferences(tc)
+		assert prefs.pkgconfig_mode == .dynamic, tc.raw_ccompiler
+		assert prefs.compile_values['v:static_pkgconfig'] == 'false', tc.raw_ccompiler
+		assert prefs.file_defines == [], tc.raw_ccompiler
+		assert 'v_static_pkgconfig' !in prefs.source_file_defines(), tc.raw_ccompiler
+	}
+}
+
+fn test_static_pkgconfig_mode_excludes_clang_msvc_driver_dialect() {
+	for tc in [
+		StaticPkgConfigPrefCase{
+			raw_ccompiler: 'clang-cl.exe'
+			ccompiler:     'clang'
+			cflags:        ['-static']
+		},
+		StaticPkgConfigPrefCase{
+			raw_ccompiler: 'C:/msys64/ucrt64/bin/x86_64-w64-mingw32-clang-cl.exe'
+			ccompiler:     'clang'
+			cflags:        ['-static']
+		},
+		StaticPkgConfigPrefCase{
+			raw_ccompiler: 'clang.exe'
+			ccompiler:     'clang'
+			cflags:        ['--driver-mode=cl', '-static']
+		},
+		StaticPkgConfigPrefCase{
+			raw_ccompiler: 'clang.exe'
+			ccompiler:     'clang'
+			cflags:        ['-static']
+			env_ldflags:   ['--driver-mode=cl']
+		},
+	] {
+		prefs := static_pkgconfig_preferences(tc)
+		assert prefs.pkgconfig_mode == .dynamic, tc.raw_ccompiler
+		assert prefs.compile_values['v:static_pkgconfig'] == 'false', tc.raw_ccompiler
+	}
+	gnu_clang := static_pkgconfig_preferences(StaticPkgConfigPrefCase{
+		raw_ccompiler: 'clang.exe'
+		ccompiler:     'clang'
+		cflags:        ['--driver-mode=cl-like', '-static']
+	})
+	assert gnu_clang.pkgconfig_mode == .static_
+}
+
+fn test_static_pkgconfig_reresolution_replaces_compiler_owned_state() {
+	mut prefs := new_preferences()
+	prefs.ccompiler = 'gcc'
+	prefs.resolve_pkgconfig_mode('gcc', ['-static'], [], [], [])
+	assert prefs.pkgconfig_mode == .static_
+	assert prefs.compile_values['v:static_pkgconfig'] == 'true'
+	assert prefs.file_defines == ['v_static_pkgconfig']
+	prefs.ccompiler = 'tinyc'
+	prefs.resolve_pkgconfig_mode('tcc', ['-static'], [], [], [])
+	assert prefs.pkgconfig_mode == .dynamic
+	assert prefs.compile_values['v:static_pkgconfig'] == 'false'
+	assert prefs.file_defines == []
+}
+
+fn test_static_pkgconfig_file_define_selects_only_the_internal_variant() {
+	root := os.join_path(os.vtmp_dir(), 'v3_static_pkgconfig_files_${os.getpid()}')
+	os.rmdir_all(root) or {}
+	os.mkdir_all(root) or { panic(err) }
+	defer {
+		os.rmdir_all(root) or {}
+	}
+	for file in ['bindings.v', 'bindings_d_v_static_pkgconfig.v',
+		'bindings_notd_v_static_pkgconfig.v'] {
+		os.write_file(os.join_path_single(root, file), 'module bindings\n') or { panic(err) }
+	}
+	dynamic := static_pkgconfig_preferences(StaticPkgConfigPrefCase{
+		raw_ccompiler: 'gcc'
+		ccompiler:     'gcc'
+	})
+	static_prefs := static_pkgconfig_preferences(StaticPkgConfigPrefCase{
+		raw_ccompiler: 'gcc'
+		ccompiler:     'gcc'
+		cflags:        ['-static']
+	})
+	assert get_v_files_from_dir_for_target(root, dynamic.source_file_defines(), host_target()).map(os.base(it)) == [
+		'bindings.v',
+		'bindings_notd_v_static_pkgconfig.v',
+	]
+	assert get_v_files_from_dir_for_target(root, static_prefs.source_file_defines(), host_target()).map(os.base(it)) == [
+		'bindings.v',
+		'bindings_d_v_static_pkgconfig.v',
+	]
+}
