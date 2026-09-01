3d689214ea2f95a139e87852f6ac7fba417e1b2b
GGRei <112974093+GGRei@users.noreply.github.com>
2026-09-01 12:38:39 +0200

    v3: prefix GNU function attributes and invalidate cache

--- vlib/v3/gen/c/attributes_test.v
+++ vlib/v3/gen/c/attributes_test.v
@@ -1,5 +1,6 @@
 module c
 
+import os
 import v3.flat
 import v3.token
 import v3.types
@@ -33,10 +34,49 @@ fn test_noinline_attribute_is_preserved_for_generic_specialization() {
 		'noinline',
 	]
 
-	assert g.fn_decl_c_attribute(template_id) == ' __attribute__((noinline))'
-	assert g.fn_decl_c_attribute(specialization_id) == ' __attribute__((noinline))'
+	assert g.fn_decl_gnu_attribute_prefix(template_id) == '__attribute__((noinline)) '
+	assert g.fn_decl_gnu_attribute_prefix(specialization_id) == '__attribute__((noinline)) '
 
 	g.ccompiler = 'msvc'
-	assert g.fn_decl_c_attribute(specialization_id) == ''
+	assert g.fn_decl_gnu_attribute_prefix(specialization_id) == ''
 	assert g.fn_decl_msvc_noinline_prefix(specialization_id) == '__declspec(noinline) '
+}
+
+fn test_function_definition_attributes_are_composed_as_prefixes() {
+	mut g := cgen_attribute_test_gen()
+	g.ccompiler = 'clang'
+	fn_id := g.a.add_node(flat.Node{
+		kind: .fn_decl
+		value: 'helper'
+	})
+	g.decl_attrs[int(fn_id)] = ['_constructor', '_destructor', 'noinline']
+
+	gnu_prefix := g.fn_decl_gnu_attribute_prefix(fn_id)
+	assert gnu_prefix == '__attribute__((constructor, destructor, noinline)) '
+	definition := '${gnu_prefix}__attribute__((visibility("default"))) VNORETURN int helper(void) {'
+	assert definition == '__attribute__((constructor, destructor, noinline)) __attribute__((visibility("default"))) VNORETURN int helper(void) {'
+	ordinary_definition := '${gnu_prefix}int helper(void) {'
+	assert !ordinary_definition.contains(') __attribute__')
+	assert !definition.contains('helper(void) __attribute__')
+
+	source := os.read_file(os.join_path(os.dir(@FILE), 'fn.v'))!
+	start := source.index('g.write(g.fn_decl_gnu_attribute_prefix(node_id))') or {
+		panic('ordinary function definition does not emit the GNU attribute prefix')
+	}
+	end_offset := source[start..].index("g.writeln(') {')") or {
+		panic('ordinary function definition terminator is missing')
+	}
+	callsite := source[start..start + end_offset + "g.writeln(') {')".len]
+	gnu_pos := callsite.index('g.write(g.fn_decl_gnu_attribute_prefix(node_id))') or { -1 }
+	msvc_pos := callsite.index('g.write(g.fn_decl_msvc_noinline_prefix(node_id))') or { -1 }
+	export_pos := callsite.index('g.write(g.exported_symbol_attribute())') or { -1 }
+	noreturn_pos := callsite.index('g.write(g.fn_decl_noreturn_prefix(node_id))') or { -1 }
+	type_pos := callsite.index('g.write(g.fn_return_type_name(ret_type))') or { -1 }
+	assert gnu_pos >= 0
+	assert gnu_pos < msvc_pos
+	assert msvc_pos < export_pos
+	assert export_pos < noreturn_pos
+	assert noreturn_pos < type_pos
+	assert callsite.count('fn_decl_gnu_attribute_prefix') == 1
+	assert !callsite.contains('fn_decl_c_attribute')
 }
